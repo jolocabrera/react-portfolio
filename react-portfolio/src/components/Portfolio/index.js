5f68ed6d1450f54acf588f40f602fce6d8f070a8
jolocabrera <jolocabrera@yahoo.com>
2022-09-11 14:39:56 -0700

    edited list of feature projects

--- react-portfolio/src/components/Portfolio/index.js
+++ react-portfolio/src/components/Portfolio/index.js
@@ -12,26 +12,33 @@ function Portfolio(props) {
       languages: 'MongoDB/Express.js/React/Node.js'
     },
     {
-      name: 'Game Quest',
-      id: 'game-quest',
-      link: 'https://game-quest-v01.herokuapp.com/',
-      category: 'primary-app',
-      languages: 'MongoDB/Express.js/React/Node.js'
+      name: 'Pet Park',
+      id: 'pet-park',
+      link: 'https://radiant-eyrie-35854.herokuapp.com/',
+      category: 'secondary-app',
+      languages: 'HTML/CSS/JS/Jquery/Node/Express/Sequelize/MySQL/Phaser/Tiled'
     },
     {
-      name: 'Game Quest',
-      id: 'game-quest',
-      link: 'https://game-quest-v01.herokuapp.com/',
-      category: 'primary-app',
-      languages: 'MongoDB/Express.js/React/Node.js'
+      name: 'Park Buddy',
+      id: 'park-buddy',
+      link: 'https://magoofy.github.io/project-1',
+      category: 'secondary-app',
+      languages: 'HTML/CSS/JS/BootStrap/API'
     },
     {
-      name: 'Game Quest',
-      id: 'game-quest',
-      link: 'https://game-quest-v01.herokuapp.com/',
-      category: 'primary-app',
-      languages: 'MongoDB/Express.js/React/Node.js'
+      name: 'Run Buddy',
+      id: 'run-buddy',
+      link: 'https://jolocabrera.github.io/run-buddy/',
+      category: 'secondary-app',
+      languages: 'HTML/CSS'
     },
+    {
+      name: 'Password Generator',
+      id: 'password-generator',
+      link: '',
+      category: 'secondary-app',
+      languages: 'HTML/CSS/JS'
+    }
   ]);
 
 
