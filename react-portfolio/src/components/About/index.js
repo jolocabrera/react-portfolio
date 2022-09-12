f9bae362e95921dfea2d59d7a09ff3c6edabe9a1
Jolo <jolocabrera@yahoo.com>
2022-09-12 12:18:34 -0700

    edited about me

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -9,8 +9,9 @@ function About() {
           I am currently a graduating senior at San Jose State University, set to graduate Spring 2022.
           I am a general business major but also have a knack for programming due to my affinity for problem solving.
           In my tenor at San Jose State, although I am a General Business Major, I have geared my elective focus toward MIS based courses to fulfill my hunger for problem solving.
-          Throughout these times I have added a number of skills to my repertoire such as experience in Salesforce, Python, Microsoft Word, Excel, and PowerPoint.
-          I am currently enrolled in UC Berkeley's Coding Boot Camp to further my knowledge in the field and add to my list of skills and abilities.
+          Throughout these times I have added a number of skills to my repertoire such as experience in Salesforce, Python, and Microsoft Suite.
+          <br/><br/>
+          I have recently graduated from UC Berkeley's Full Stack Coding Boot Camp and am now proficient in a number of back end tools such as Express.js, MongoDB,Node.js, and GraphQL, as well as front end tools such as HTML, CSS, and React.
           <br/><br/>
           I am also the director of a dance company based in Daly City, CA. As a director of a dance company I am able to practice a number of other helpful skills such as team management, logistics, attention to detail, as well as exercise my creative mind.
           <br/><br/>
